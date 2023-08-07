2688cdafa97307f6edf1455067971c60fa245054
FelipeAviani <felipeaviani@gmail.com>
2023-08-07 11:51:37 -0300

    prueba render

--- src/db.js
+++ src/db.js
@@ -43,7 +43,7 @@ const {
 //     }
 //   );
 
-const sequelize = new Sequelize(DB_URL, {
+const sequelize = new Sequelize(DB_RENDER, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
   dialectOptions:{
