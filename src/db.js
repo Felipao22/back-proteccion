74eab9eecc4c99c7cfde39dfd8f6959f08318613
Felipao22 <felipeaviani@gmail.com>
2023-08-31 11:41:36 -0300

    change db in render

--- src/db.js
+++ src/db.js
@@ -41,6 +41,7 @@ const {
 
 
 // deploy
+
 const sequelize = new Sequelize(DB_RENDER, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
