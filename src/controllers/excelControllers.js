012a1a242ce5756802182608c8435976519dba20
Felipao22 <felipeaviani@gmail.com>
2025-10-21 20:04:39 -0300

    feat: implemento función para que los archivos se guarden y no se sobrescriban

--- src/controllers/excelControllers.js
+++ src/controllers/excelControllers.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 const ExcelJS = require("exceljs");
 const excelToPdf = require("./pdfControllers");
 const { File, User } = require("../db");
+const formatDate = require("../utils/formatDate");
 
 const generateVisitExcel = async (req, res) => {
   try {
@@ -93,14 +94,7 @@ const generateVisitExcel = async (req, res) => {
       wrapText: true,
     };
 
-    // Formatear nombre con fecha (DD/MM/AA)
-    const date = new Date();
-    const day = String(date.getDate()).padStart(2, "0");
-    const month = String(date.getMonth() + 1).padStart(2, "0");
-    const year = String(date.getFullYear()).slice(-2);
-    const formattedDate = `${day}-${month}-${year}`;
-
-    const outputFileName = `Constancia-de-Visita-${formattedDate}.xlsx`;
+    const outputFileName = `Constancia-de-Visita-${formatDate()}.xlsx`;
 
     const outputDir = path.join("uploads");
     fs.mkdirSync(outputDir, { recursive: true });
